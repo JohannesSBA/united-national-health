4a31ced8787ae255d7b105d839194b8fecbb48c8
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2025-11-14 13:27:32 -0500

    Update ESLint configuration to integrate Prettier and add custom rules; enhance package.json with new test scripts and additional dependencies for testing and linting.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,41 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import next from "eslint-config-next";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintConfig = [
+  ...next,
+  {
+    ...prettierRecommended,
+    name: "custom:prettier",
+    rules: {
+      ...prettierRecommended.rules,
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "all",
+          semi: true,
+          tabWidth: 2,
+          singleQuote: false,
+          printWidth: 80,
+          endOfLine: "auto",
+          arrowParens: "always",
+        },
+        {
+          usePrettierrc: false,
+        },
+      ],
+    },
+  },
+  {
+    name: "custom:ignores",
+    ignores: [
+      "generated/**",
+      "**/.turbo/**",
+      "**/*.d.ts",
+      "next.config.ts",
+      "vitest.config.ts",
+      "webpack.config.ts",
+    ],
+  },
+];
 
 export default eslintConfig;
